2e82d445d86d36d1c596bc5268d6e2ea39275647
OriolBen <oriol.ben.laravel@gmail.com>
2020-04-21 15:56:11 +0200

    3 files modified

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -31,6 +31,7 @@ import { MatTableModule } from '@angular/material/table'
 import { MatTabsModule } from '@angular/material/tabs'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatTooltipModule} from '@angular/material/tooltip'
+import { MatTreeModule } from '@angular/material/tree'
 
 @NgModule({
   imports: [
@@ -65,6 +66,7 @@ import { MatTooltipModule} from '@angular/material/tooltip'
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
+    MatTreeModule
   ],
   exports: [
     MatAutocompleteModule,
@@ -96,6 +98,8 @@ import { MatTooltipModule} from '@angular/material/tooltip'
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
+    MatTreeModule
   ]
 })
-export class MaterialModule { }
+
+export class MaterialModule {}
