99d8054dadd9d155b08492363b9c06368bbe8344
OriolBen <oriol.ben.laravel@gmail.com>
2020-04-23 19:47:45 +0200

    6 files modified

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import {MatAutocompleteModule } from '@angular/material/autocomplete'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
 import { MatButtonModule} from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
 import { MatCheckboxModule } from '@angular/material/checkbox'
